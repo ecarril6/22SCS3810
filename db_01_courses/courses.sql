453a692a2c08e404df22419f47c67d07a5717b41
Thyago Mota <thyagomota@gmail.com>
2022-02-21 09:46:14 -0700

    update

---
+++ db_01_courses/courses.sql
@@ -0,0 +1,39 @@
+-- TODO: add a comment section with the names of the team members of the project (limited to 2). You are allowed to work solo as well. 
+
+-- TODO: create the courses database
+
+-- TODO: "open" the database for use
+
+-- TODO: (optional) drop all tables
+
+-- TODO: create table instructors
+
+-- TODO: create table courses
+
+-- TODO: create table sections
+
+-- TODO: manually insert a few instructors
+
+-- TODO: manually insert a few courses
+
+-- TODO: manually insert a few sections
+
+-- TODO: the total number of courses (name the count as "total")
+
+-- TODO: a list of all courses prefix, number, and title, sorted by prefix and then number
+
+-- TODO: an alphabetical list of all instructors in the database
+
+-- TODO: the prefix, number, section, and (course) title of all courses sections in the database, sorted by prefix, number and section
+
+-- TODO: the prefix, number, the number of sections (named as "sections"), and (course) title of all courses sections in the database, sorted by prefix and number
+
+-- TODO: an alphabetical list of the instructors that are teaching CS 1050 or CS 2050 (must avoid showing names repeated)
+
+-- TODO: show an alphabetical list of instructors followed by the number of sections (named as "sections") that they are teaching, sorted in descending order of "sections"
+
+-- TODO: same as before, but limit the output to the top 3 instructors based on the number of sections that they are teaching
+
+-- TODO: show an alphabetical list of the instructor(s) that are NOT currently teaching a section this semester 
+
+-- TODO: show the sections (with the instructor assigned to them) of CS 1050 that are being offered in the spring (2022) on TR 10:00am-11:50am
